4d6827637f2ba8e9adc30d07f030864dfd7e9984
Nikhil Thomas <nikhil@arceoanalytics.com>
2020-09-02 17:35:43 -0400

    Weeks 3 and 4
    Signed-off-by: Nikhil Thomas <nikhil@arceoanalytics.com>

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,75 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// infinite loop
+// read from keyboard
+// if equal to zero, goto WHITEFILL
+// goto BLACKFILL
+// both WHITEFILL and BLACKFILL goto main loop
+
+// fillrow
+// for i = 0; i < 256; i++
+// 	for j = 0; j < 32; j++
+// 	RAM[address] = fillvalue
+// 	address = address + 1
+
+(READ)
+	// our fill mechanics need to loop so let's handle that
+	@idx
+	M = 0
+	// initialize base address
+	@SCREEN
+	D = A
+	@address
+	M = D
+
+	@KBD
+	D = M
+	// if KBD is zero, empty so whitefill
+	@WHITEFILL
+	D; JEQ
+
+	@BLACKFILL
+	0; JMP
+
+(WHITEFILL)
+	@fill
+	M = 0
+
+	@FILL
+	0; JMP
+
+(BLACKFILL)
+	@fill
+	M = -1
+
+	@FILL
+	0; JMP
+
+(FILL)
+	// get fill value
+	@fill
+	D = M
+	
+	// fill location at address
+	@address
+	A = M
+	M = D
+
+	// increment address
+	@address
+	M = M + 1
+	D = M
+	
+	// check if we've written entire value
+	@KBD
+	D = A - D
+	
+	// if D is zero, we filled entire screen
+	// therefore we go back to read
+	@READ
+	D; JEQ
+
+	@FILL
+	0; JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,53 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// pseudocode
+// R2 = 0
+// i = r1
+// for i = 0; i < r0; i++
+//   r2 = r2 + r1
+
+    @R2
+    M = 0
+
+    // hold onto R1 in m
+    @R1
+    D = M
+    @m
+    M = D
+
+    // loop max, R0 = n
+    @R0
+    D = M
+    @n
+    M = D
+    @i
+    M = 0
+
+(LOOP)
+    // first, set up goto when we reach end
+    @i
+    D = M
+    @n
+    D = D - M
+    // when D is zero, we've run n times
+    @END
+    D; JEQ
+
+    // increment counter
+    @i
+    M = M + 1
+
+    // get multiplier val
+    @m
+    D = M
+    
+    // add it to running sum in R2
+    @R2
+    M = M + D
+    @LOOP
+    0; JMP
+
+(END)
+    @END
+    0; JMP
